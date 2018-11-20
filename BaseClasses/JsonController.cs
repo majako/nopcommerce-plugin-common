b811dda7b07178b6ca84a97b1b34208ee75b4183
Martin Gustafsson <gusten86@gmail.com>
2018-11-20 11:39:43 +0100

    feat(all): add JsonController

---
+++ BaseClasses/JsonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Nop.Web.Framework.Controllers;
+
+namespace Majako.Plugin.Misc.Common.BaseClasses
+{
+   public class JsonController : BasePluginController
+    {
+        protected new IActionResult Ok(object data)
+        {
+            return JsonResult(data, (int)HttpStatusCode.OK);
+        }
+
+        private IActionResult JsonResult(object data, int? statusCode)
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var jsonResult = new ContentResult
+            {
+                Content = JsonConvert.SerializeObject(data, jsonSerializerSettings),
+                ContentType = "application/json",
+                StatusCode = statusCode
+            };
+
+            return jsonResult;
+        }
+    }
+}
