2b3ccbaeb89128eb6992c1cff5cecb99c0396285
Martin Gustafsson <gusten86@gmail.com>
2019-11-20 15:19:56 +0100

    feat(CamleCaseJsonResultFilter): use filter for camel case result

--- Filters/CamelCaseJsonResultFilter.cs
+++ Filters/CamelCaseJsonResultFilter.cs
@@ -1,32 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System.Buffers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace Majako.Plugin.Common.Filters
 {
-    public class CamelCaseJsonResultFilter : IAsyncResultFilter
+    public class CamelCaseAttribute : ActionFilterAttribute
     {
-        private readonly JsonSerializerSettings _globalSettings;
-
-        public CamelCaseJsonResultFilter(IOptions<MvcJsonOptions> optionsAccessor)
+        public override void OnActionExecuted(ActionExecutedContext ctx)
         {
-            _globalSettings = optionsAccessor.Value.SerializerSettings;
-        }
-
-        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
-        {
-            var settings = _globalSettings;
-            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-
-            if (context.Result is JsonResult originResult)
+            if (ctx.Result is ObjectResult objectResult)
             {
-                context.Result = new JsonResult(originResult.Value, settings);
+                objectResult.Formatters.Add(new JsonOutputFormatter(
+                    new JsonSerializerSettings
+                    {
+                        ContractResolver = new DefaultContractResolver
+                        {
+                            NamingStrategy = new CamelCaseNamingStrategy()
+                        }
+                    },
+                    ctx.HttpContext.RequestServices.GetRequiredService<ArrayPool<char>>()));
             }
-
-            await next();
         }
     }
 }
