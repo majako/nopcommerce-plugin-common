a547cf420db7c7d7d2b21e1623bb327bcaa28519
Martin Gustafsson <gusten86@gmail.com>
2019-12-15 17:03:29 +0100

    refactor(Constants): add constants file

---
+++ Infrastructure/Constants.cs
@@ -0,0 +1,12 @@
+namespace Majako.Plugin.Common.Infrastructure
+{
+    public class Constants
+    {
+        public class LicenseAttributeNames
+        {
+            public const string NopCommerceVersion = "nopcommerce version";
+            public const string License = "license";
+            public const string StoreUrl = "store url";
+        }
+    }
+}
