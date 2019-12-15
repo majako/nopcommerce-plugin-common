b2bbec99ce96d27f98cfcb4585a749791331a9d7
Martin Gustafsson <gusten86@gmail.com>
2019-12-15 17:02:34 +0100

    feat(LicenseService): add download license method

--- Services/ILicenseService.cs
+++ Services/ILicenseService.cs
@@ -1,9 +1,11 @@
-﻿using Portable.Licensing;
+﻿using System.Threading.Tasks;
+using Portable.Licensing;
 
 namespace Majako.Plugin.Common.Services
 {
     public interface ILicenseService
     {
         License GetValidLicense(string licenseXmlString, string publicKey, string pluginSystemName);
+        Task<string> DownloadLicense(string signature);
     }
 }
--- Services/LicenseService.cs
+++ Services/LicenseService.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Majako.Plugin.Common.Infrastructure;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using Nop.Core;
 using Nop.Core.Domain.Logging;
+using Nop.Services.Configuration;
 using Nop.Services.Logging;
 using Portable.Licensing;
 using Portable.Licensing.Validation;
@@ -14,14 +20,17 @@ namespace Majako.Plugin.Common.Services
     {
         private readonly ILogger _logger;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ISettingService _settingService;
 
         public LicenseService(
             ILogger logger,
-            IHttpContextAccessor httpContextAccessor
+            IHttpContextAccessor httpContextAccessor,
+            ISettingService settingService
             )
         {
             _logger = logger;
             _httpContextAccessor = httpContextAccessor;
+            _settingService = settingService;
         }
 
         public License GetValidLicense(string licenseXmlString, string publicKey, string pluginSystemName)
@@ -42,26 +51,28 @@ namespace Majako.Plugin.Common.Services
                     sb.AppendLine(failure.GetType().Name + ": " + failure.Message + " - " + failure.HowToResolve);
                 }
 
-                if (validationFailures.Any()) return null;
-
-                if (license.Expiration < DateTime.UtcNow)
+                if (validationFailures.Any())
                 {
-                    sb.AppendLine("License expired. Please update license: https://www.majako.net");
+                    if (sb.Length > 0)
+                    {
+                        _logger.InsertLog(LogLevel.Warning, $"Invalid license for plugin: {pluginSystemName}", sb.ToString());
+                    }
+                    return null;   
                 }
 
-                if (license.AdditionalAttributes.Get("nopcommerce-version") != NopVersion.CurrentVersion)
+                if (license.AdditionalAttributes.Get(Constants.LicenseAttributeNames.NopCommerceVersion) != NopVersion.CurrentVersion)
                 {
                     sb.AppendLine("Invalid License for this nopCommerce version. Please get a valid license: https://www.majako.net");
                 }
 
-                if (license.AdditionalAttributes.Get("multi-store") != "true")
+                if (license.AdditionalAttributes.Get(Constants.LicenseAttributeNames.License) != "multiple domain")
                 {
                     var host = _httpContextAccessor.HttpContext.Request.Host;
 
                     var currentHost = host.Host.Replace("www.", "");
-                    var licensedHost = license.AdditionalAttributes.Get("host");
+                    var licensedHost = license.AdditionalAttributes.Get("store url");
 
-                    if (licensedHost != currentHost && currentHost != "localhost")
+                    if (!string.Equals(currentHost, licensedHost, StringComparison.InvariantCultureIgnoreCase) && currentHost != "localhost")
                     {
                         sb.AppendLine($"Invalid License for current host {currentHost}. Licensed host is {licensedHost}. Please get a valid license: https://www.majako.net");
                     }
@@ -72,7 +83,7 @@ namespace Majako.Plugin.Common.Services
                     _logger.InsertLog(LogLevel.Warning, $"Invalid license for plugin: {pluginSystemName}", sb.ToString());
                     return null;
                 }
-
+            
                 return license;
             }
             catch (Exception e)
@@ -80,6 +91,22 @@ namespace Majako.Plugin.Common.Services
                 _logger.Error("Error validating license", e);
 
                 return null;
+            }
+        }
+        
+        public async Task<string> DownloadLicense(string signature)
+        {
+            if(string.IsNullOrEmpty(signature)) throw  new ArgumentNullException(nameof(signature));
+
+            var licenseDomainSetting = _settingService.GetSetting("Majako.Plugin.Common.LicenseDomain");
+            var licenseDomain = licenseDomainSetting == null ? "https://www.majako.net" : licenseDomainSetting.Value;
+
+            using (var client = new HttpClient())
+            {
+                // TODO make domain a setting
+                var response = await client.GetAsync($"{licenseDomain}/Plugins/Licensing/GetLicense?signature={WebUtility.UrlEncode(signature)}");
+
+                return await response.Content.ReadAsStringAsync();
             }
         }
     }
