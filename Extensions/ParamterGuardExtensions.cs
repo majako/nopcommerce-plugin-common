8650484d7f7b7c9cd4b30c9d3b5d5d989dd5389b
Surda <surda.kontakt@gmail.com>
2020-07-10 13:57:26 +0200

    Added schedule task utilities

---
+++ Extensions/ParamterGuardExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Majako.Plugin.Common.Extensions
+{
+    public static class ParamterGuardExtensions
+    {
+        public static void NotNull<T>(this T source, string parameterName) where T : class
+        {
+            parameterName.NotEmpty(nameof(parameterName));
+            if (source == null)
+                throw new ArgumentNullException(parameterName);
+        }
+
+        public static void NotEmpty(this string source, string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(parameterName))
+                throw new ArgumentException(nameof(parameterName));
+            
+            if (string.IsNullOrWhiteSpace(source))
+                throw new ArgumentException(parameterName);
+        }
+        public static void NotEmpty(this string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                throw new ArgumentException($"Input is empty!");
+        }
+    }
+}
---
+++ Infrastructure/ScheduleTasks/ScheduleTasksRegistrator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Majako.Plugin.Common.Extensions;
+using Nop.Core.Domain.Tasks;
+using Nop.Services.Tasks;
+
+namespace Majako.Plugin.Common.Infrastructure.ScheduleTasks
+{
+    public static class ScheduleTasksRegistrator
+    {
+        public static void RegisterScheduleTasks(this IScheduleTaskService source, Assembly assembly)
+        {
+            source.NotNull(nameof(source));
+            assembly.NotNull(nameof(assembly));
+
+            foreach (var task in GetScheduleTasks(assembly))
+            {
+                var scheduledTask = new ScheduleTask()
+                {
+                    Enabled = task.Item2.IsEnabled,
+                    Name = task.Item2.Name,
+                    Seconds = (int)task.Item2.Interval.TotalSeconds,
+                    StopOnError = task.Item2.StopOnError,
+                    Type = task.Item1.FullName
+                };
+
+                source.InsertTask(scheduledTask);
+            }
+        }
+
+        public static void DeregisterScheduleTasks(this IScheduleTaskService source, Assembly assembly)
+        {
+            source.NotNull(nameof(source));
+            assembly.NotNull(nameof(assembly));
+
+            foreach(var task in GetScheduleTasks(assembly))
+            {
+                var foundTask = source.GetTaskByType(task.Item1.FullName);
+                if (foundTask == null)
+                    continue;
+                source.DeleteTask(foundTask);
+            }
+        }
+
+        private static IEnumerable<ValueTuple<Type, RegistredScheduleTaskAttribute>> GetScheduleTasks(Assembly assembly)
+        {
+            foreach(var type in assembly.GetTypes().Where(c => c.IsAssignableFrom(typeof(IScheduleTask))))
+            {
+                var attribute = type.GetCustomAttribute<RegistredScheduleTaskAttribute>();
+                if (attribute == null)
+                    continue;
+                yield return new ValueTuple<Type, RegistredScheduleTaskAttribute>(type, attribute);
+            }
+        }
+    }
+}
