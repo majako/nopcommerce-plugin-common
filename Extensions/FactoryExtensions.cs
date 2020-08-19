d75ea10346870659ff0f46879fa6b3a85e01f5b7
Surda <surda.kontakt@gmail.com>
2020-08-19 14:34:29 +0200

    Factory base classes

---
+++ Extensions/FactoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Majako.Plugin.Common.Abstractions.Factories;
+
+namespace Majako.Plugin.Common.Extensions
+{
+    public static class FactoryExtensions
+    {
+        public static void AddFactory<TFactory, TFrom, TDestination>(this ContainerBuilder source) where TFactory : IFactory<TFrom, TDestination>
+        {
+            source.RegisterType<TFactory>()
+                .As<IFactory<TFrom, TDestination>>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
