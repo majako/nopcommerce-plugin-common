b540128a0d8e20f84bff24172f519e86205aec42
dat-preciofishbone <dat.nguyen.tan@preciofishbone.se>
2020-12-28 17:19:30 +0700

    add data provider extension

---
+++ Extensions/NopDataProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Nop.Core.Infrastructure;
+using Nop.Data;
+
+namespace Majako.Plugin.Common.Extensions
+{
+    public static class NopDataProviderExtensions
+    {
+        public static void ExecuteNonQueryFromFile(this INopDataProvider dataProvider, string filePath)
+        {
+            var fileProvider = EngineContext.Current.Resolve<INopFileProvider>();
+            filePath = fileProvider.MapPath(filePath);
+            if (!fileProvider.FileExists(filePath))
+                return;
+
+            dataProvider.ExecuteNonQuery(fileProvider.ReadAllText(filePath, Encoding.Default));
+        }
+    }
+}
