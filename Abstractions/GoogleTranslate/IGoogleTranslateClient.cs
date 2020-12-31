8729e0d194b884823118fa1a9bd05c735800412d
dat-preciofishbone <dat.nguyen.tan@preciofishbone.se>
2020-12-31 15:47:44 +0700

    move goolge service to image provider plugin to fix reflection error

--- Abstractions/GoogleTranslate/IGoogleTranslateClient.cs
+++ Abstractions/GoogleTranslate/IGoogleTranslateClient.cs
@@ -6,7 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Majako.Plugin.Common.Extensions;
 
-namespace Majako.Plugin.Common.Abstraction.GoogleTranslate
+namespace Majako.Plugin.Common.Abstractions.GoogleTranslate
 {
     public interface IGoogleTranslateClient
     {
